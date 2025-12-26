0c64fca6f29b81a044525de42cc2d6670a2935ee
w4d32001 <freedom01022001@gmail.com>
2025-12-26 12:10:53 -0500

    pruebita

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from "../components/login-form/login-form";
 import { LogoComponent } from "../../../shared/components/logo/logo";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [LoginFormComponent, LogoComponent],
+  imports: [LoginFormComponent, LogoComponent, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })

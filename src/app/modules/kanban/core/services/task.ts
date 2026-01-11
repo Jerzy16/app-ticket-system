882af8e96f989e8427293e99f9b10e2407476018
w4d32001 <freedom01022001@gmail.com>
2026-01-11 15:55:00 -0500

    fix: correcion de mover tareas

--- src/app/modules/kanban/core/services/task.ts
+++ src/app/modules/kanban/core/services/task.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
 import { Task, TaskCreateDto } from '../models/task/task.model';
 import { ApiResponse } from '../../../../shared/interfaces/api-response.interface';
@@ -25,19 +25,21 @@ export class TaskService {
         fromBoardId: string,
         toBoardId: string,
         position: number = 0
-    ): Observable<ApiResponse<Task>> {
-        const params = new HttpParams()
-            .set('fromBoardId', fromBoardId)
-            .set('toBoardId', toBoardId)
-            .set('newIndex', position.toString());
+    ): Observable<Task> {
+        const body = {
+            fromBoardId,
+            toBoardId,
+            newIndex: position
+        };
 
         return this.http.patch<ApiResponse<Task>>(
             `${this.url}/tasks/${taskId}/move`,
-            null,
-            { params }
+            body
+        ).pipe(
+            map(response => response.data),
+            catchError(this.handleError)
         );
     }
-
     getTaskById(taskId: string): Observable<ApiResponse<Task>> {
         return this.http.get<ApiResponse<Task>>(`${this.url}/tasks/${taskId}`);
     }
@@ -52,5 +54,9 @@ export class TaskService {
 
     getTasksByUser(userId: string): Observable<ApiResponse<Task[]>> {
         return this.http.get<ApiResponse<Task[]>>(`${this.url}/tasks/user/${userId}`);
+    }
+    private handleError(error: any) {
+        console.error('Error en TaskService:', error);
+        return throwError(() => error);
     }
 }
